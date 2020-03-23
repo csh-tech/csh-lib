88415989fe1fd0d08ef2d8702cbd5e32ac9ab244
chenshanhui <1065521866@qq.com>
2020-03-23 17:53:12 +0800

    add quick_sort

---
+++ quick_sort.cc
@@ -0,0 +1,37 @@
+#include "quick_sort.h"
+
+void QuickSort::quickSort(int low, int high) {
+  if (low < high) {
+      int boundary = partition(low, high);
+      quickSort(low, boundary - 1);
+      quickSort(boundary + 1, high);
+  }
+}
+
+void QuickSort::printVector() {
+  for (int i = 0; i < nums_.size(); i++) {
+    std::cout << nums_[i] << std::endl;
+  }
+}
+
+int QuickSort::partition(int low, int high) {
+  int pivot = nums_[high];
+  int i = low - 1;
+  for (int j = low; j < high; j++) {
+    if (nums_[j] < pivot) {
+        i += 1;
+        std::swap(nums_[i], nums_[j]);
+    }
+  }
+  std::swap(nums_[i+1], nums_[high]);
+  return (i+1);
+}
+
+int main() {
+  std::vector<int> test_list = {2, 2, 1, 1, 4, 3, 99, 1111, 0, 5};
+  QuickSort sort(test_list);
+  sort.quickSort(0, test_list.size() - 1);
+  sort.printVector();
+
+  return 0;
+}
---
+++ quick_sort.h
@@ -0,0 +1,13 @@
+#include <vector>
+#include <iostream>
+
+class QuickSort {
+public:
+  QuickSort(std::vector<int>& nums) : nums_(nums) {}
+  void quickSort(int low, int high);
+  void printVector();
+
+private:
+  int partition(int low, int high);
+  std::vector<int> nums_;
+};
