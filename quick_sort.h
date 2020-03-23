3a436ce118b5e3e1941e2a7f5c7e87eccc6d5955
chenshanhui <1065521866@qq.com>
2020-03-23 18:02:55 +0800

    fix quick_sort.h

--- quick_sort.h
+++ quick_sort.h
@@ -4,10 +4,13 @@
 class QuickSort {
 public:
   QuickSort(std::vector<int>& nums) : nums_(nums) {}
+
   void quickSort(int low, int high);
+
   void printVector();
 
 private:
   int partition(int low, int high);
+  
   std::vector<int> nums_;
 };
